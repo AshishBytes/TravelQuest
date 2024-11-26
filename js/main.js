0982750cd1cf51019956626139a723c83dc3f921
shailendra mourya <shailendramourya17@gmail.com>
2024-11-26 16:42:43 +0530

    Welcome Page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-window.onload = function() {
-  alert('Welcome to TravelQuest!');
-};
+// window.onload = function() {
+//   alert('Welcome to TravelQuest!');
+// };
+
+
+// let currentSlide = 0;
+//         const slides = document.querySelector('.slides');
+//         const totalSlides = document.querySelectorAll('.slide').length;
+
+//         document.getElementById('next').addEventListener('click', () => {
+//             currentSlide = (currentSlide + 1) % totalSlides;
+//             updateSlidePosition();
+//         });
+
+//         document.getElementById('prev').addEventListener('click', () => {
+//             currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+//             updateSlidePosition();
+//         });
+
+//         function updateSlidePosition() {
+//             const slideWidth = document.querySelector('.slide').clientWidth;
+//             slides.style.transform = 'translateX(' + (-slideWidth * currentSlide) + 'px)';
+//         }
